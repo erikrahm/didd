4993d3f34e4585853ce4b9c832996b5bbbdcd214
Duncan Krassikoff <duncan@krassikoff.com>
2016-06-01 19:29:35 -0500

    add gulp sass, re-org folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // gulpfile.js
 var gulp        = require('gulp');
 var gutil       = require('gulp-util');
+var gsass       = require('gulp-sass');
 var source      = require('vinyl-source-stream');
 var babelify    = require('babelify');
 var watchify    = require('watchify');
@@ -8,6 +9,37 @@ var browserify  = require('browserify');
 var browserSync = require('browser-sync').create();
 
 watchify.args.debug = true;
+
+//config
+var client = './src/client/';
+var server = './src/server/';
+var dist = './dist/';
+var temp = '.temp';
+
+var config = {
+  client: client,
+  server: server,
+  build: dist,
+  temp: temp,
+  sass: client + 'sass/',
+  css: dist + 'css/'
+};
+
+gulp.task('sass-watcher', [], function() {
+  gulp.watch(config.sass + '*.*', ['sass']);
+});
+
+gulp.task('sass', [], function() {
+  gutil.log('Compiling Sass to CSS');
+  
+  return gulp
+    .src(config.sass + 'config.scss')
+      //.pipe($.plumber())
+      .pipe(gsass().on('error', gsass.logError))
+      .pipe(gsass({outputStyle: 'expanded'}))
+      //.pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+      .pipe(gulp.dest(config.css));
+});
 
 // dat bundler
 var bundler = watchify(browserify('./src/scripts/script.js', watchify.args));
